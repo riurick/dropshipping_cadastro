168f998a869ae2a5ab76ab320050fdb1b45c7c5b
Rodrigo <rodrigo.mss01@gmail.com>
2019-03-21 01:26:42 -0300

    autenticacao

--- src/main/java/com/dropshipping/DropshippingCadastrpApplication.java
+++ src/main/java/com/dropshipping/DropshippingCadastrpApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication(scanBasePackages={"com.dropshipping"})
-public class DropshippingApplication {
+public class DropshippingCadastrpApplication {
 	public static void main(String[] args) {
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.enableDefaultTyping();
-		SpringApplication.run(DropshippingApplication.class, args);
+		SpringApplication.run(DropshippingCadastrpApplication.class, args);
 	}
 
 
